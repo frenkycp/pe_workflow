b4b0fa98ab2790235a60be55f18b4ec489c671df
Frenky Cahya Purnama <friendq.c.p@gmail.com>
2017-04-06 08:35:32 +0700

    - user profile cannot change name

--- views/site/profile.php
+++ views/site/profile.php
@@ -39,7 +39,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     <p>
                         <input type="tel" hidden /> <!-- disable chrome autofill -->
                         <?= $form->field($model, 'password')->passwordInput(['maxlength' => true, 'autocomplete' => "off"]) ?>
-                        <?= $form->field($model, 'name')->textInput(['maxlength' => true, 'style' => 'text-transform: uppercase']) ?>
+                        <?= $form->field($model, 'name')->textInput(['maxlength' => true, 'style' => 'text-transform: uppercase', 'readonly' => true]) ?>
                         <?= $form->field($model, 'photo_url')->widget(\kartik\file\FileInput::className(), [
                             'options' => ['accept' => 'image/*'],
                             'pluginOptions' => [
